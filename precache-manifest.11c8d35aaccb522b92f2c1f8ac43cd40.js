0ccdfecaac022760c3eef39eade996728defd48d
vyasishanatc194 <vyasishanatc194@gmail.com>
2020-08-12 09:25:24 +0530

    Deployed Signup form

---
+++ precache-manifest.11c8d35aaccb522b92f2c1f8ac43cd40.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf5b67d2ccd5cd8b41309612ca52d03",
+    "url": "/reactsignup/index.html"
+  },
+  {
+    "revision": "d4289016af6eff60da39",
+    "url": "/reactsignup/static/css/2.b21f750b.chunk.css"
+  },
+  {
+    "revision": "3fad6f5ef094115fe551",
+    "url": "/reactsignup/static/css/main.530b5742.chunk.css"
+  },
+  {
+    "revision": "d4289016af6eff60da39",
+    "url": "/reactsignup/static/js/2.7009f672.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/reactsignup/static/js/2.7009f672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fad6f5ef094115fe551",
+    "url": "/reactsignup/static/js/main.ab6731d1.chunk.js"
+  },
+  {
+    "revision": "e4f11cff18d21cf926c8",
+    "url": "/reactsignup/static/js/runtime-main.f91c1724.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/reactsignup/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/reactsignup/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/reactsignup/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/reactsignup/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/reactsignup/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/reactsignup/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/reactsignup/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/reactsignup/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/reactsignup/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/reactsignup/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/reactsignup/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/reactsignup/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/reactsignup/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/reactsignup/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/reactsignup/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/reactsignup/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
